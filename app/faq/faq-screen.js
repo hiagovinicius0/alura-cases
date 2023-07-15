5759eb8de98ff1bc2ee299078e324439efa504b4
Hiago Vinícius <hiago149@gmail.com>
2023-07-15 14:00:33 -0300

    add force cache

--- app/faq/faq-screen.js
+++ app/faq/faq-screen.js
@@ -7,7 +7,7 @@ export async function getFaq() {
 	console.log('Em modo DEV, sempre roda! A cada acesso')
 	console.log('Roda SOMENTE em build time')
 	const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
-	return fetch(FAQ_API_URL)
+	return fetch(FAQ_API_URL, { cache: 'force-cache' })
 		.then((respostaDoServidor) => {
 			return respostaDoServidor.json();
 		})
